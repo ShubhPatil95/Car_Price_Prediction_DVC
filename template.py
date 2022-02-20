4cef1b94287d48a9f601886a081e6040b56f1ac8
Shubham Patil <shubham.patil@cimcondigital.com>
2022-02-20 13:22:50 +0530

    Data added to DVC

---
+++ template.py
@@ -0,0 +1,23 @@
+import os
+dirs=[
+     "data_given",
+     os.path.join("data", "raw") ,
+     os.path.join("data", "processed"),
+     "notebook",
+     "saved_models",
+     "src"]
+
+for dir in dirs:
+    os.makedirs(dir,exist_ok=True)
+    with open(os.path.join(dir,".gitkeep"),"w") as f:
+        pass
+    
+files=[
+      "dvc.yaml",
+      "params.yaml",
+      ".gitignore",
+      os.path.join("src","__init__.py") ]
+
+for file in files:
+    with open(file,"w") as f:
+        pass
