24f0aa533e00916ec2aa474a8bc48f54f0408cff
Shubham Patil <shubham.patil@cimcondigital.com>
2022-02-21 14:47:40 +0530

    pytest has been added

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,7 +3,7 @@
 import json
 import logging
 import os
-import joblib
+# import joblib
 import pytest
 from prediction_service.prediction import form_response, api_response
 import prediction_service
