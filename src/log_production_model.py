f69725b553e52c23487cbf2df3ad6596d9fe4105
Shubham Patil <shubham.patil@cimcondigital.com>
2022-02-22 17:08:10 +0530

    Mlflow Done

---
+++ src/log_production_model.py
@@ -0,0 +1,72 @@
+import argparse
+import mlflow
+from mlflow.tracking import MlflowClient
+from pprint import pprint
+import joblib
+import os
+import yaml
+
+
+def read_file(file_path):
+    with open(file_path) as yaml_file:
+        file=yaml.safe_load(yaml_file)
+    return file
+
+def log_production_model(config_path):
+    config = read_file(config_path)
+    
+    
+    mlflow_config = config["mlflow_config"] 
+    
+
+    model_name = mlflow_config["registered_model_name"]
+
+
+    remote_server_uri = mlflow_config["remote_server_uri"]
+    
+    mlflow.set_tracking_uri(remote_server_uri)
+    
+    runs = mlflow.search_runs(experiment_ids=1)
+        
+    ####### mention you metrics name checking its name in mlflow dashboard#########
+    
+    lowest = runs["metrics.training_mae"].sort_values(ascending=True)[0]
+    
+    lowest_run_id = runs[runs["metrics.training_mae"] == lowest]["run_id"][0]
+    
+    client = MlflowClient()
+    
+    
+    for mv in client.search_model_versions(f"name='{model_name}'"):
+        mv = dict(mv)
+        
+        if mv["run_id"] == lowest_run_id:
+            current_version = mv["version"]
+            logged_model = mv["source"]
+            print(logged_model)
+            pprint(mv, indent=4)
+            client.transition_model_version_stage(
+                name=model_name,
+                version=current_version,
+                stage="Production"
+            )
+        else:
+            current_version = mv["version"]
+            client.transition_model_version_stage(
+                name=model_name,
+                version=current_version,
+                stage="Staging"
+            )        
+
+    loaded_model = mlflow.pyfunc.load_model(logged_model)
+    
+    model_path = config["webapp_model_dir"] #"prediction_service/model"
+
+    joblib.dump(loaded_model, model_path)
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args = args.parse_args()
+    data = log_production_model(config_path=parsed_args.config)
